6beb275e08a60a36b8d01c830f53139e6a8f0481
miguelmc0217 <miguelmc0217@users.noreply.github.com>
2019-04-10 15:47:02 -0400

    dB

---
+++ src/apis/Base.Api/Context/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Base.Api.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Base.Api.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Prestamo> Prestamos { set; get; }
+        public DbSet<Solicitud> Solicitudes { set; get; }
+     }
+}
---
+++ src/apis/Base.Api/Migrations/20190410190453_Initial.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Base.Api.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Prestamos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nombre = table.Column<string>(nullable: true),
+                    Apellido = table.Column<string>(nullable: true),
+                    Monto = table.Column<double>(nullable: false),
+                    CuotasVal = table.Column<int>(nullable: false),
+                    CuotasQ = table.Column<string>(nullable: true),
+                    CuotasAlready = table.Column<int>(nullable: false),
+                    CuotasPending = table.Column<int>(nullable: false),
+                    Status = table.Column<string>(nullable: true),
+                    Payment = table.Column<string>(nullable: true),
+                    Ends = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prestamos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Solicitudes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nombre = table.Column<string>(nullable: true),
+                    Apellido = table.Column<string>(nullable: true),
+                    Correo = table.Column<string>(nullable: true),
+                    Telefono = table.Column<string>(nullable: true),
+                    Celular = table.Column<string>(nullable: true),
+                    Electoral = table.Column<string>(nullable: true),
+                    Empresa = table.Column<string>(nullable: true),
+                    Sueldo = table.Column<double>(nullable: false),
+                    Ingresos = table.Column<double>(nullable: false),
+                    NombreGarante = table.Column<string>(nullable: true),
+                    ApellidoGarante = table.Column<string>(nullable: true),
+                    CorreoGar = table.Column<string>(nullable: true),
+                    TelefonoGar = table.Column<string>(nullable: true),
+                    CelularGar = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Solicitudes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Prestamos");
+
+            migrationBuilder.DropTable(
+                name: "Solicitudes");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
